a38447bcb569a68292ae1826441f4e14aff570b5
laberion <laberion.ajvazi@studentet.uni-pr.edu>
2020-12-23 21:47:58 +0100

    slider button functionality

--- scripts/home.js
+++ scripts/home.js
@@ -13,8 +13,30 @@ var sliderElements = [
 var slider = document.getElementById('slider');
 var sliderImg = document.getElementById('sliderImg');
 var imgControls = document.querySelector("#imgControls");
+var rightArrow = document.getElementById('rightArrow');
+var leftArrow = document.getElementById('leftArrow');
+
+rightArrow.addEventListener('click', () => {
+    clearInterval(loopSlider);
+    changeSlide();
+    loopSlider = setInterval(changeSlide,3000);
+});
+
+leftArrow.addEventListener('click', () => {
+    clearInterval(loopSlider);
+    if(index - 2 >= -2){
+        index -=2 ;
+    }
+    if(index < -1){
+        index = sliderElements.length-2;
+    }
+    console.log(index);
+    changeSlide();
+    loopSlider = setInterval(changeSlide,3000);
+});
 
 for(var i = 0 ; i< sliderElements.length ; i++){
+    let actualIndex = i;
     var svg = document.createElement("svg");
     svg.innerHTML = `
         <svg  class="imgAnchor" width="50" height="50">
@@ -22,6 +44,12 @@ for(var i = 0 ; i< sliderElements.length ; i++){
         </svg>
     `;
     imgControls.appendChild(svg);
+    svg.addEventListener('click', () => {
+        clearInterval(loopSlider);
+        index = actualIndex-1;
+        changeSlide();
+        loopSlider = setInterval(changeSlide,3000);
+    });
 }
 
 var index = 0;
@@ -36,7 +64,6 @@ function changeSlide() {
         image[0].className = "show";
          }, 801);
          
-    console.log(index);
     if(index === sliderElements.length-1) { index = 0;}
     else{index ++;}
 }
